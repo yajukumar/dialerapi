bcb4e6b48d65e10d50c304531f50907251efa48b
yajuvendra <yajuvendra.kumar@stashfin.com>
2022-01-25 12:12:56 +0530

    test

--- classes/update.php
+++ classes/update.php
@@ -5,7 +5,6 @@ namespace Stratum\classes;
 class Update{
 
     public function updateComposer(){
-        echo 'hi';exit;
         echo shell_exec('composer update --working-dir=/var/www/html/stratum/api');
     }
 }
